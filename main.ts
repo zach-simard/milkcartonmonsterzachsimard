38f571bfeecc69dbecd08b5d597ef23ba0bfb887
zachary ladouceur-simard <118993100+zach-simard@users.noreply.github.com>
2023-01-10 10:50:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,49 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    pins.servoWritePin(AnalogPin.P0, 0)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, 180)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(400)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(300)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(400)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+})
+let angle = 0
+pins.setAudioPin(AnalogPin.P0)
+let opened = 95
+let closed = 175
+basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    0
+    )
+    angle = pins.map(
+    input.lightLevel(),
+    0,
+    255,
+    opened,
+    closed
+    )
+    pins.servoWritePin(AnalogPin.P0, angle)
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.ThreeG)) {
+        music.playMelody("B A G A G F A C5 ", 268)
+    }
+})
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P0, input.lightLevel())
+})
